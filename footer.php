c6d0ac3d082c4e0b2a3405b3b97249e4778c4440
iSeFz <seifyahia@gmail.com>
2024-04-14 19:51:49 +0200

    Added header.php & footer.php files

---
+++ footer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <footer>&copy; 2024 FCAI Information Systems Department Students. All rights reserved.</footer>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>Registraion Form - Web-based Information Systems Project</header>
+</body>
+
+</html>
