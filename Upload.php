ecd008a10a9b8dbd0a071aae4111f070236fa89c
Nour Tarek <nourtarrek.2@gmail.com>
2024-04-22 15:34:27 +0200

    Update Upload.php

--- Upload.php
+++ Upload.php
@@ -1,36 +1,31 @@
 <?php
-function uploadImage(){
-    if(isset($_POST['submit']))
-    {
-        $check = getimagesize($_FILES['photo']['tmp_name']);
-        if($check === false){
-            echo "not an Image";
+function uploadImage()
+{
+    $imagesFolderName = "./photos/";
+    $check = getimagesize($_FILES['photo']['tmp_name']);
+    if ($check === false) {
+        echo "not an Image";
+    } else {
+        $image = $_FILES["photo"]["tmp_name"];
+        $data = file_get_contents($image);
+        $name = $_FILES["photo"]["name"];
+        while (file_exists("photos/" . $name)) {
+            $imageName = getNameFromImageName($name);
+            $extension = getExtensionFromImageName($name);
+            $imageName++;
+            $name = $imageName . $extension;
         }
-        else{
-            $image = $_FILES["photo"]["tmp_name"];
-            $data = file_get_contents($image);
-            $name = $_FILES["photo"]["name"];
-            while(file_exists("photos/".$name)){
-                $imageName = getNameFromImageName($name);
-                $extension = getExtensionFromImageName($name);
-                $imageName++;
-                $name = $imageName.$extension;
-            }
-            file_put_contents("photos/".$name, $data);
-            return $name;
-            //set name in database
-        }
-    }
-    else
-    {
-        echo"Error";
+        $name = $imagesFolderName . $name;
+        file_put_contents($name, $data);
+        return $name;
     }
 }
 
-function getNameFromImageName($name){
+function getNameFromImageName($name)
+{
     $newName = "";
-    for($i= 0;$i<strlen($name);$i++){
-        if($name[$i]== "."){
+    for ($i = 0; $i < strlen($name); $i++) {
+        if ($name[$i] == ".") {
             break;
         }
         $newName .= $name[$i];
@@ -38,17 +33,17 @@ function getNameFromImageName($name){
     return $newName;
 }
 
-function getExtensionFromImageName($name){
+function getExtensionFromImageName($name)
+{
     $extension = "";
     $found = false;
-    for($i= 0;$i<strlen($name);$i++){
-        if($name[$i] == '.'){
+    for ($i = 0; $i < strlen($name); $i++) {
+        if ($name[$i] == '.') {
             $found = true;
         }
-        if($found){
+        if ($found) {
             $extension .= $name[$i];
         }
     }
     return $extension;
 }
-?>
