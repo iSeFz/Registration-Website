aeb317405266418742d317b87ec6f34422894ec4
Nour Wael Elshaer <101601441+nourelshaer@users.noreply.github.com>
2024-05-08 20:21:36 +0300

    delete old registration-website (w/o vendor)

--- registration-website/resources/views/footer.blade.php
+++
@@ -1,1 +0,0 @@
-<footer>&copy; 2024 FCAI Information Systems Department Students. All rights reserved.</footer>
--- registration-website/resources/views/header.blade.php
+++
@@ -1,1 +0,0 @@
-<header>Registraion Form - Web-based Information Systems Project</header>
